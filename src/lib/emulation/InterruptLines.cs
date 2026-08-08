4935e4feb0058e8004c37489adc446e7c9c40e85
Bryan Gonzalez <bgonza868@gmail.com>
2026-08-08 14:07:10 -0400

    fix: additional nmi conformance 2 tests

--- src/lib/emulation/InterruptLines.cs
+++ src/lib/emulation/InterruptLines.cs
@@ -2,14 +2,46 @@ namespace SR.Emulation.Nes;
 
 public sealed class InterruptLines
 {
-    private bool _irq;
+    private bool _nmiEdgePending;
+    private int _nmiEdgeAgePpuDots;
 
-    public bool Nmi { get; set; }
+    public bool Nmi
+    {
+        get;
+        set
+        {
+            if (value && !field)
+            {
+                _nmiEdgePending = true;
+                _nmiEdgeAgePpuDots = 0;
+            }
+            field = value;
+        }
+    }
+
+    internal bool ConsumeNmiEdge()
+    {
+        var pending = _nmiEdgePending;
+        _nmiEdgePending = false;
+        return pending;
+    }
+
+    internal void AdvancePpuDot()
+    {
+        if (_nmiEdgePending) _nmiEdgeAgePpuDots++;
+    }
+
+    internal void CancelShortNmiEdge(int minimumPpuDots)
+    {
+        if (_nmiEdgePending && _nmiEdgeAgePpuDots < minimumPpuDots)
+            _nmiEdgePending = false;
+    }
+
     internal bool DelayNmiOneInstruction { get; set; }
     public bool Irq
     {
-        get => _irq || MapperIrq;
-        set => _irq = value;
+        get => field || MapperIrq;
+        set;
     }
 
     internal bool MapperIrq { get; set; }
